d7fe0469f4df12cd110b9ea2a385bd76d6a6aa1a
Darby09 <165986512+Darby09@users.noreply.github.com>
2024-06-12 02:05:18 +0900

    Update UserServiceImpl.java

--- src/main/java/pet_studio/pet_studio_spring/service/UserServiceImpl.java
+++ src/main/java/pet_studio/pet_studio_spring/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package pet_studio.pet_studio_spring.service;
 
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Streamable;
 import org.springframework.http.HttpStatus;
@@ -9,34 +8,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import pet_studio.pet_studio_spring.domain.Image;
 import pet_studio.pet_studio_spring.domain.User;
-import pet_studio.pet_studio_spring.dto.follow.FollowerDto;
-import pet_studio.pet_studio_spring.dto.follow.FollowingDto;
 import pet_studio.pet_studio_spring.dto.user.UserDto;
 import pet_studio.pet_studio_spring.dto.mypage.UserProfileDto;
-import pet_studio.pet_studio_spring.dto.user.UserFollowListDto;
-import pet_studio.pet_studio_spring.exception.CustomException;
-import pet_studio.pet_studio_spring.exception.ErrorCode;
-import pet_studio.pet_studio_spring.repository.ImageRepository;
 import pet_studio.pet_studio_spring.repository.UserRepository;
+
 import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import static pet_studio.pet_studio_spring.domain.FollowStatus.FOLLOWING;
-import static pet_studio.pet_studio_spring.exception.ErrorCode.ALREADY_EXIST_NICKNAME;
-import static pet_studio.pet_studio_spring.exception.ErrorCode.ALREADY_EXIST_USER;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private ImageRepository imageRepository;
-    @Autowired
     private ImageService imageService;
-    @Autowired
-    private FollowService followService;
 
     public User findUserById(String id) {
         Optional<User> user = userRepository.findByUserId(id);
@@ -64,7 +50,6 @@ public class UserServiceImpl implements UserService {
         Image image = Image.builder()
                 .url("/profileImages/ic_account.png")
                 .user(user)
-                .type("profile")
                 .build();
         userRepository.save(user);
 
@@ -108,24 +93,18 @@ public class UserServiceImpl implements UserService {
 
     public ResponseEntity<?> myPageMain(@PathVariable("userId") String userId){
         Optional<User> optionalUser = userRepository.findByUserId(userId);
-
-        if (optionalUser.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (!optionalUser.isPresent()) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
-        int followingCnt = followService.followings(optionalUser.get().getUserNo());
-        int followerCnt = followService.followers(optionalUser.get().getUserNo());
-
-
 
         User user = optionalUser.get();
-
         UserProfileDto result = new UserProfileDto();
         result.setUserId(user.getUserId());
         result.setNickName(user.getNickName());
         result.setUserImageUrl(user.getImg());
         result.setIntroduce(user.getIntroduce());
-        result.setFollowerCnt(followerCnt);
-        result.setFollowingCnt(followingCnt);
+        result.setFromMeToOthersCnt(0);
+        result.setToMeFromOthersCnt(0);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
@@ -154,37 +133,5 @@ public class UserServiceImpl implements UserService {
             return true; // 업데이트 성공
         }
         return false; // 사용자를 찾지 못함
-    }
-
-    // 팔로우,팔로잉 목록 조희
-    public UserFollowListDto getFollowList(String userId) {
-        User user = findUserById(userId);
-
-        List<FollowingDto> followingDTOs = user.getFollowingList().stream()
-                .filter(follow -> follow.getStatus() == FOLLOWING)
-                .map(FollowingDto::convertToDTO)
-                .toList();
-
-        List<FollowerDto> followerDTOs = user.getFollowerList().stream()
-                .filter(follow -> follow.getStatus() == FOLLOWING)
-                .map(FollowerDto::convertToDTO)
-                .toList();
-
-        return UserFollowListDto.builder()
-                .followerList(followerDTOs)
-                .followingList(followingDTOs)
-                .build();
-    }
-
-    public void validateUserId(String userId) {
-        if (userId.length() < 7) {
-            throw new CustomException(ErrorCode.INVALID_USER_ID);
-        }
-    }
-
-    public void validateUserPw(String password) {
-        if (password.length() < 7) {
-            throw new CustomException(ErrorCode.INVALID_USER_PW);
-        }
     }
 }
